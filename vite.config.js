0be10f67ce511742444ded5d2660b01f9a0d23fc
roscibely <rosanacibely@gmail.com>
2025-08-31 09:36:56 -0300

    refactor: change base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cilab-ufersa.github.io/',
+  base: '/',
   build: {
     outDir: 'dist',
   }
